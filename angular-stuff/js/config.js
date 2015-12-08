c05eefd552ac1727b27794b6b2af61115b706dff
teo <teo.ferizovic@hotmail.com>
2015-12-08 10:55:14 +0100

    Removed files

--- angular-stuff/js/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-angular.module('config', [])
-
-.constant('ENV', 'local')
-
-.constant('CONFIG', {API_URL:'https://api.github.com/users/peterbe'})
-
-;
--- angular-stuff/js/directive/directive.js
+++
@@ -1,5 +0,0 @@
-app.directive('myCustomer', function() {
-    return {
-      template: 'ja sam direktivaaa'
-    };
-});
